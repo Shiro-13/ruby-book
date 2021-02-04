85b09584a69a59715b79cc7be0a0481fe9020409
Shiro-13 <shirai0119@gmail.com>
2021-02-04 17:40:09 +0900

    RGB変換プログラム_to_hexメソッド作成

---
+++ ruby-book/lib/rgb.rb
@@ -0,0 +1,10 @@
+def to_hex(r, g, b)
+  # '#' +
+  # r.to_s(16).rjust(2, '0') +
+  # g.to_s(16).rjust(2, '0') +
+  # b.to_s(16).rjust(2, '0')
+  # リファクタリング後
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
+end
