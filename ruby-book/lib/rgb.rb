3cd519919ff3029220973af03cc8cf39decb90fd
Shiro-13 <shirai0119@gmail.com>
2021-02-10 19:57:16 +0900

    RGB変換PG_to_intsメソッド作成

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -1,10 +1,17 @@
 def to_hex(r, g, b)
-  # '#' +
-  # r.to_s(16).rjust(2, '0') +
-  # g.to_s(16).rjust(2, '0') +
-  # b.to_s(16).rjust(2, '0')
-  # リファクタリング後
   [r, g, b].inject('#') do |hex, n|
     hex + n.to_s(16).rjust(2, '0')
   end
+end
+
+def to_ints(hex)
+  # リファクタリング前
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # ints = []
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+  hex.scan(/\w\w/).map(&:hex)
 end
